54e99572d5431fb7e3cd78cbb745f7533c852173
yuribadz <yuriy.badziuk@posteo.de>
2019-07-29 23:52:55 +0300

    Setup kerrisk echo fork server

---
+++ inc/EchoServ.hpp
@@ -0,0 +1,59 @@
+#ifndef ECHOSERV_HPP
+#define ECHOSERV_HPP
+#include <syslog.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>                   
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netdb.h>
+
+
+
+
+class EchoServ
+{
+public:
+	static constexpr const int BUF_SIZE = 4096;
+	static constexpr const int BACKLOG = 10;
+	static constexpr const char* SERVICE = "echoserv";
+	static constexpr const char BD_NO_CHDIR =  01;         
+	static constexpr const char BD_NO_CLOSE_FILES = 02;    
+	static constexpr const char BD_NO_REOPEN_STD_FDS = 04; 
+	                                                       
+	static constexpr const char BD_NO_UMASK0 = 010;
+	static constexpr const short BD_MAX_CLOSE = 8192;
+                      
+
+	EchoServ(int clientFd, int socketFd);
+	int start();
+
+private:
+	int becomeDaemon(int flags);
+	int inetConnect(const char *host,
+					const char *service,
+					int type);
+	int inetListen(const char *service,
+				   int backlog,
+				   socklen_t *addrlen);
+	int inetBind(const char *service,
+				 int type,
+				 socklen_t *addrlen);
+	char *inetAddressStr(const struct sockaddr *addr,
+						 socklen_t addrlen,
+						 char *addrStr, int addrStrLen);
+	static void handleRequest(int clientFd);
+
+
+	struct sigaction m_sa;
+	int m_cfd; // Client file descriptor
+	int m_sfd; // Server file descriptor
+};
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-//cmd
+#include <iostream>
+#include <string>
+int main()
+{
+  std::string yuri;
+  yuri.append("nice");
+  std::cout << yuri << "\n";
+  return 0;
+}
+
+
