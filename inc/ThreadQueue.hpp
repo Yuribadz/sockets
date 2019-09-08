ba6e050b26e8585731fe7579a48b199593978df8
yuribadz <yuriy.badziuk@posteo.de>
2019-09-08 13:52:13 +0300

    [task] test Mutex based queue

--- inc/ThreadQueue.hpp
+++ inc/ThreadQueue.hpp
@@ -2,6 +2,7 @@
 #define THREADQUEUE_HPP
 #include <memory>
 #include <atomic>
+#include <string>
 
 template<typename T>
 class ThreadQueue
@@ -23,20 +24,22 @@ private:
 	struct node{
 		std::atomic<T*> data;
 		std::atomic<node_counter> count;
-		counted_node_ptr next;
+		std::atomic<counted_node_ptr> next;
 		node()
 		{
 			node_counter new_count;
 			new_count.internal_count = 0;
 			new_count.external_count = 2;
 			count.store(new_count);
-			next.ptr = nullptr;
-			next.external_count = 0;
+			counted_node_ptr new_next;
+			new_next.ptr = nullptr;
+			new_next.external_count = 0;
+			next.store(new_next);
 		}
 		void release_ref()
 		{
 			node_counter old_counter =
-				count.load()(std::memory_order_relaxed);
+				count.load(std::memory_order_relaxed);
 			node_counter new_counter;
 			do
 			{
@@ -81,9 +84,9 @@ private:
 		{
 			new_counter = old_counter;
 			--new_counter.external_count;
-			new_counter.internal_count + count_increase;
+			new_counter.internal_count += count_increase;
 		}
-		while(!ptr->count.compare_exhange_strong(
+		while(!ptr->count.compare_exchange_strong(
 				  old_counter, new_counter,
 				  std::memory_order_acquire,
 				  std::memory_order_relaxed));
@@ -119,9 +122,9 @@ public:
 				return std::unique_ptr<T>();
 			}
 			counted_node_ptr next = ptr->next.load();
-			if(head.compare_exchage_strong(old_head, next))
+			if(head.compare_exchange_strong(old_head, next))
 			{
-				T* const res = ptr->data.exhange(nullptr);
+				T* const res = ptr->data.exchange(nullptr);
 				free_external_counter(old_head);
 				return std::unique_ptr<T>(res);
 			}
@@ -129,43 +132,42 @@ public:
 		}
 	}
 	void push(T new_value)
-	{
-		std::unique_ptr<T> new_data(new T(new_value));
-		counted_node_ptr new_next;
-		new_next.ptr = new node;
-		new_next.external_count = 1;
-		counted_node_ptr old_tail = tail.load();
-		for(;;)
-		{
-			increase_external_count(tail, old_tail);
-			T* old_data = nullptr;
-			if(old_tail.ptr->data.compare_exhange_strong(
-				   old_data, new_data.get()))
-			{
-				counted_node_ptr old_next = {0};
-				if(!old_tail.ptr->next.compare_exchange_strong(
-					   old_next, new_next))
-				{
-					delete new_next.ptr;
-					new_next = old_next;
-				}
-			
-				set_new_tail(old_tail, new_next);
-				new_data.release();
-				break;
-			}
-			else
-			{
-				counted_node_ptr old_next = {0};
-				if(old_tail.ptr->next.compare_exchange_strong(
-					   old_next, new_next))
-				{
-					old_next = new_next;
-					new_next.ptr = new node;
-				}
-				set_new_tail(old_tail, old_next);
-			}
-		}
-	}
+    {
+        std::unique_ptr<T> new_data(new T(new_value));
+        counted_node_ptr new_next;
+        new_next.ptr = new node;
+        new_next.external_count=1;
+        counted_node_ptr old_tail=tail.load();
+        for(;;)
+        {
+            increase_external_count(tail,old_tail);
+            T* old_data=nullptr;
+            if(old_tail.ptr->data.compare_exchange_strong(
+                   old_data,new_data.get()))
+            {
+                counted_node_ptr old_next={0};
+                if(!old_tail.ptr->next.compare_exchange_strong(
+                       old_next,new_next))
+                {
+                    delete new_next.ptr;
+                    new_next=old_next;
+                }
+                set_new_tail(old_tail, new_next);
+                new_data.release();
+                break;
+            }
+            else
+            {
+                counted_node_ptr old_next={0};
+                if(old_tail.ptr->next.compare_exchange_strong(
+                       old_next,new_next))
+                {
+                    old_next=new_next;
+                    new_next.ptr=new node;
+                }
+                set_new_tail(old_tail, old_next);
+            }
+        }
+	}	
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
-#include "ThreadQueue.hpp"
+#include <string>
+#include <thread>
+#include "TsQueue.hpp"
 int main()
 {
-	std::cout << "test";
-	return 0;
+	TsQueue<int> que;
+	int count = 0;
+	auto func1 = [&que]{for(int i = 0; i < 100; ++i)
+	{
+		que.push(i);
+	}};
+	auto func2 = [&que]{for(int i = 100; i < 200; ++i)
+		{
+			que.push(i);
+		}};
+	auto popperPipe = [&que, &count]
+					  {
+						  std::this_thread::sleep_for(std::chrono::seconds(2));
+						  while(auto val = que.try_pop())
+						  {
+							  ++count;
+							  std::cout << "#"<< count <<
+								  "popped value=" << *val.get()
+										<< "\n";
+						  }
+					  };
+	std::thread thread3(popperPipe);
+	thread3.detach();
+	std::thread thread1(func1);
+	std::thread thread2(func2);
+	thread1.join();
+	thread2.join();
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+    return 0;
 }
