263fb9bba5be495eb6ac2ea19be9e16e80ece5df
Yurii Badziuk <yuriy.badziuk@posteo.de>
2021-04-10 22:24:39 +0300

    Add test for real case and temporary definitions

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,3 +1,20 @@
 #include <gtest/gtest.h>  // googletest header file
 
-TEST(sucess, sucess) { EXPECT_EQ(1, 1); }
+#include <algorithm>
+#include <iomsg.hpp>
+#include <numeric>
+#include <vector>
+
+TEST(iomsg, success_write) {
+  IoMsg msg(4, 512);
+  msg.body_len = 512;
+  std::vector<char> testData(512);
+  int pv = 0;
+  std::generate(testData.begin(), testData.end(), [&pv]() {
+    return ++pv;
+  });
+  msg.data = std::move(testData);
+  ASSERT_EQ(msg.body_len, 512);
+  ASSERT_EQ(msg.length(), 516);
+  ASSERT_EQ(msg.data.size(), 512);
+}
